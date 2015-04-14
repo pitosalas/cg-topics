bb3571c85f1b641f95e4e6f2a9281e6d62e090f1
Pito Salas <pitosalas@gmail.com>
2015-04-13 21:03:55 -0400

    X

---
+++ scripts/elevator.rb
@@ -0,0 +1,16 @@
+class Person
+	# their name
+end
+
+class Elevator
+	# List of people on board
+	# direction of motion (up, down, stopped)
+end
+
+class Floor
+	# what elevators are at this floor right now
+	# List of people waiting here
+end
+
+class Brain
+end
