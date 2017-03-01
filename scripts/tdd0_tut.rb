770b02d8b1b40add2c698b07f7134b4d46cd9a16
Pito Salas <pitosalas@gmail.com>
2017-03-01 15:18:16 -0500

    Checkpoint work to git

--- scripts/tdd0_tut.rb
+++ scripts/tdd0_tut.rb
@@ -1,27 +1,31 @@
-require 'minitest/autorun'
-
 def fact(n)
-   if n <= 1
-      1
-   else
-      fact(n-1) * n
-   end
+  if n <= 1
+    1
+  else
+    fact(n - 1) * n
+  end
 end
 
-describe "factorials" do
-   it "works for 1" do
-      fact(1).must_equal 1
-   end
+# How can I test if this works?
 
-   it "works for 2" do
-      fact(2).must_equal 2
-   end
-
-   it "works for 20" do
-      fact(20).must_equal 2432902008176640000
-   end
-
-   it "works for zero" do
-      fact(0).must_equal 0
-   end
+if (fact(1) != 1 )
+  puts "error on parm 1"
 end
+if (fact(2) != 2 )
+  puts "error on parm 2"
+end
+
+(-5..+5).each {|i| puts "factorial of #{i} i  #{fact(i)}" }
+
+# Output
+# factorial of -5 i  1
+# factorial of -4 i  1
+# factorial of -3 i  1
+# factorial of -2 i  1
+# factorial of -1 i  1
+# factorial of 0 i  1
+# factorial of 1 i  1
+# factorial of 2 i  2
+# factorial of 3 i  6
+# factorial of 4 i  24
+# factorial of 5 i  120
