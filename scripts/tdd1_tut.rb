20e63972b4ef5d9c31dbe29c1263297f74ea68cc
Pito Salas <pitosalas@gmail.com>
2017-07-26 16:08:57 -0400

    i dont know why all files were dirty. this is a test

--- scripts/tdd1_tut.rb
+++ scripts/tdd1_tut.rb
@@ -1,27 +1,27 @@
 require 'minitest/autorun'
 
 def fact(n)
-  if n == 1
-    1
-  else
-    fact(n - 1) * n
-  end
+	if n == 1
+		1
+	else 
+		fact(n-1) * n
+	end
 end
 
-describe 'factorials' do
-  it 'works for 1' do
-    fact(1).must_equal 1
-  end
+describe "factorials" do
+	it "works for 1" do
+		fact(1).must_equal 1
+	end
 
-  it 'works for 2' do
-    fact(2).must_equal 2
-  end
+	it "works for 2" do
+		fact(2).must_equal 2
+	end
 
-  it 'works for 20' do
-    fact(20).must_equal 2_432_902_008_176_640_000
-  end
+	it "works for 20" do
+		fact(20).must_equal 2432902008176640000
+	end
 
-  it 'works for zero' do
-    fact(0).must_equal 0
-  end
+	it "works for zero" do
+		fact(0).must_equal 0
+	end
 end
