84caaaa2bd700ea02f69af295c529d24d431b7da
Pito Salas <pitosalas@gmail.com>
2020-01-10 19:49:45 -0500

    checkpoint

--- scripts/dbl_demo1.rb
+++ scripts/dbl_demo1.rb
@@ -1,22 +1,18 @@
 require 'minitest/autorun'
 
 class Book
-	attr_accessor :title, :description
+  attr_accessor :title, :description
 
-	def initialize title, author
-		binding.pry
-		@description = "Written by #{author.name}"
-	end
-
+  def initialize _title, author
+    @description = "Written by #{author.name}"
+  end
 end
 
 describe Book do
-
-	it "uses author's name in description" do
-		tim_author = MiniTest::Mock.new
-		tim_author.expect(:name, "Tim Hickey")
-		book = Book.new("Introduction to 3D graphics", author: tim_author)
-		book.description.must_equal 'Written by Tim Hickey'
-		#tim_author.verify
-	end
+  it "uses author's name in description" do
+    tim_author = MiniTest::Mock.new
+    tim_author.expect(:name, "Tim Hickey")
+    book = Book.new("Introduction to 3D graphics", author: tim_author)
+    book.description.must_equal 'Written by Tim Hickey'
+  end
 end
