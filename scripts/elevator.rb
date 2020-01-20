1fb8f9715ea776d60a313a22e547d94c1b2d12ad
Pito Salas <pitosalas@gmail.com>
2020-01-20 12:53:36 -0500

    checkpoint

--- scripts/elevator.rb
+++ scripts/elevator.rb
@@ -1,16 +1,22 @@
 class Person
-	# their name
+  # their name
 end
 
 class Elevator
-	# List of people on board
-	# direction of motion (up, down, stopped)
+  # List of people on board
+  # direction of motion (up, down, stopped)
 end
 
 class Floor
-	# what elevators are at this floor right now
-	# List of people waiting here
+  # what elevators are at this floor right now
+  # List of people waiting here
+  a= 1+ 1
+
 end
+
+a = {a: 100, b: 200, c: 300, a1: 100, b1: 200, c1: 300, a2: 100, b2: 200, c2: 300, a3: 100, b3: 200 c3: 300, a4: 100, b4: 200, c4: 300, a5: 100, b5: 200, c5: 300, a6: 100, b6: 200, c6: 300, a7: 100, b7: 200, c7: 300}
+
+puts a.inspect
 
 class Brain
 end
